6567dc5e1b289c60f8103df6d29591ae39aa8398
Andraelja <eljapramaandra@gmail.com>
2025-11-19 02:01:08 +0700

    perbaikan create task

--- backend-assesment/app/Services/TaskService.php
+++ backend-assesment/app/Services/TaskService.php
@@ -13,9 +13,9 @@ class TaskService
         // untuk mengetahui siapa yang membuat task
         $data['creator_id'] = $user->id;
         // saat membuat task pertama kali, status todo, dan firstOrFail itu untuk melempar jika tidak ada status maka error
-if (!isset($data['status_id'])) {
-    $data['status_id'] = Status::where('name', 'To Do')->firstOrFail()->id;
-}
+        if (!isset($data['status_id'])) {
+            $data['status_id'] = Status::where('name', 'To Do')->firstOrFail()->id;
+        }
 
         // jika tidak ada yang menugaskan ke dia, maka task tersebut untuk dia sendiri
         if(!isset($data['assignee_id'])) {
